d665c2a4a14d318acc6a24d587583cfc5a1a3eb5
aehamdy <ae.hamdy91@gmail.com>
2024-05-14 15:19:13 +0300

    build: FeaturesSection component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import BrowseByCategory from "./components/BrowseByCategory";
+import FeaturesSection from "./components/FeaturesSection";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import TopHeader from "./components/TopHeader";
@@ -9,6 +10,7 @@ function App() {
       <TopHeader />
       <Header />
       <BrowseByCategory />
+      <FeaturesSection />
       <Footer />
     </>
   );
