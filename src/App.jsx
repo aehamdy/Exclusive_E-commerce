87847684edb0cdc8507dfe45645a660a0e5011a8
aehamdy <ae.hamdy91@gmail.com>
2024-05-15 17:16:47 +0300

    build: Banner of hero section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import BrowseByCategory from "./components/BrowseByCategory";
 import FeaturesSection from "./components/FeaturesSection";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import HeroBanner from "./components/HeroBanner";
 import NewArrival from "./components/NewArrival";
 import TopHeader from "./components/TopHeader";
 
@@ -11,7 +12,7 @@ function App() {
     <>
       <TopHeader />
       <Header />
-      <Banner />
+      <HeroBanner />
       <Footer />
     </>
   );
